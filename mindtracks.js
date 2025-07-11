1c2b526a371b8de5920e26cf70fb2d10fb3c31fa
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-11 10:49:42 +0300

    Add files via upload

---
+++ mindtracks.js
@@ -0,0 +1,76 @@
+const links = {
+  infinityedge: [
+    "https://www.infinityedge.online/2025/03/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_11.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_86.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_48.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_13.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_83.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/2030.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/xxi.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_42.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.infinityedge.online/2025/03/blog-post_63.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  regalspace: [
+    "https://www.regalspace.online/2025/03/xxi.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_11.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_58.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/luxury.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_44.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_65.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_73.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/blog-post_1.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.regalspace.online/2025/03/2025.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  legacypath: [
+    "https://www.legacypath.online/2024/11/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_83.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_71.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_90.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/5_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  nobleway: [
+    "https://www.nobleway.online/2025/03/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_11.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_49.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/xxi.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_19.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_55.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_0.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_1.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobleway.online/2025/03/blog-post_90.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  jettrue: [
+    "https://www.jettrue.ru/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/truejet.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_67.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_81.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_32.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_79.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_87.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/truejet.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post_22.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.jettrue.ru/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ]
+};
+
+function getRandomUrl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for (const domain in links) {
+  const iframe = document.createElement('iframe');
+  iframe.src = getRandomUrl(links[domain]);
+  iframe.width = 1;
+  iframe.height = 1;
+  iframe.style.border = "0";
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+}
