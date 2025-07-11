27961b050ecc235bb340f5f1f475d0b316bcb2bd
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-11 10:17:02 +0300

    Add files via upload

---
+++ questsky.js
@@ -0,0 +1,76 @@
+const links = {
+  supremenet: [
+    "https://www.supremenet.online/2024/11/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/10-2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/seo.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_30.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_20.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_33.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_37.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.supremenet.online/2024/11/blog-post_36.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  ultrapeak: [
+    "https://www.ultrapeak.online/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_11.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_85.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_68.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_0.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_38.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_33.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_23.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ultrapeak.online/2024/09/blog-post_91.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  naturecore: [
+    "https://www.naturecore.ru/2025/02/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_58.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_34.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_85.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_88.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_13.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_39.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_40.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.naturecore.ru/2025/02/blog-post_28.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  finvektor: [
+    "https://www.finvektor.ru/2025/02/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_54.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_99.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_20.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_25.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/2025.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/blog-post_36.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.finvektor.ru/2025/02/2025_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  grandsphere: [
+    "https://www.grandsphere.online/2023/05/digital-nomad-lifestyle-how-to-work-and.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/destination-spotlight-exploring-hidden.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/remote-work-revolution-embracing.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/packing-tips-for-nomads-essential-gear.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/finding-perfect-work-life-balance-as.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/nomadic-entrepreneurship-building-and.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/slow-travel-embracing-journey-and.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/financial-strategies-for-sustainable.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/health-and-wellness-on-road-self-care.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.grandsphere.online/2023/05/the-nomadic-community-connecting-and.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ]
+};
+
+function getRandomUrl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for (const domain in links) {
+  const iframe = document.createElement('iframe');
+  iframe.src = getRandomUrl(links[domain]);
+  iframe.width = 1;
+  iframe.height = 1;
+  iframe.style.border = "0";
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+}
