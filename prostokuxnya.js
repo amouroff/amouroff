fc32f01ae4ea70012016f94f59e9c9ffb2f56444
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-11 10:43:22 +0300

    Add files via upload

---
+++ prostokuxnya.js
@@ -0,0 +1,76 @@
+const links = {
+  legacyspot: [
+    "https://www.legacyspot.online/2025/03/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_11.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_32.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/xxi.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_76.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_61.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_75.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_26.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_17.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacyspot.online/2025/03/blog-post_22.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  opuselite: [
+    "https://www.opuselite.online/2024/08/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_27.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_79.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_24.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_95.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_68.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_46.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_77.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.opuselite.online/2024/08/blog-post_90.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  majesticway: [
+    "https://www.majesticway.online/2024/08/python.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/10-javascript.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/sql.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/blog-post_20.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/blog-post_52.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/blog-post_97.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/ai.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.majesticway.online/2024/08/5-2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  skilllift: [
+    "https://www.skilllift.ru/2025/02/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/blog-post_72.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/2025.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/blog-post_88.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/vs.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/hr.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.skilllift.ru/2025/02/blog-post_16.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  nobletouch: [
+    "https://www.nobletouch.online/2024/09/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_26.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_73.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_34.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_79.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_14.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_4.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_59.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nobletouch.online/2024/09/blog-post_39.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ]
+};
+
+function getRandomUrl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for (const domain in links) {
+  const iframe = document.createElement('iframe');
+  iframe.src = getRandomUrl(links[domain]);
+  iframe.width = 1;
+  iframe.height = 1;
+  iframe.style.border = "0";
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+}
