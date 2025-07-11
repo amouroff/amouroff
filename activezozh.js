ea51a0c4de88c286888ccd5bad72033cb0d5862d
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-11 10:10:09 +0300

    Add files via upload

---
+++ activezozh.js
@@ -0,0 +1,76 @@
+const links = {
+  toproute: [
+    "https://www.toproute.ru/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_22.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_31.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_62.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_89.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_16.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_59.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_74.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.toproute.ru/2024/09/blog-post_52.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  gadgetika: [
+    "https://www.gadgetika.ru/2024/09/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_26.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_70.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_40.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_87.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_24.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_33.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.gadgetika.ru/2024/09/blog-post_93.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  nextstartup: [
+    "https://www.nextstartup.ru/2024/09/waveup.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_22.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_59.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_50.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_37.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/waveup_22.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_29.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.nextstartup.ru/2024/09/blog-post_81.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  ideiveka: [
+    "https://www.ideiveka.ru/2024/10/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_14.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_90.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_96.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/5_14.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_32.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/blog-post_85.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.ideiveka.ru/2024/10/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ],
+  budtech: [
+    "https://www.budtech.ru/2023/09/ai.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/6g.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/iot.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/xr.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post_13.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post_44.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post_67.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post_83.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.budtech.ru/2023/09/blog-post_82.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+  ]
+};
+
+function getRandomUrl(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+for (const domain in links) {
+  const iframe = document.createElement('iframe');
+  iframe.src = getRandomUrl(links[domain]);
+  iframe.width = 1;
+  iframe.height = 1;
+  iframe.style.border = "0";
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
+}
