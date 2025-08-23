c7d5d61ef4942a25afa4f00a8ec1ffc34e4455bf
amouroff <48030869+amouroff@users.noreply.github.com>
2025-08-23 23:11:03 +0300

    Update anti-iframe.js

--- anti-iframe.js
+++ anti-iframe.js
@@ -1,7 +1,3 @@
-(function() {
-    if (window.top === window.self) {
-        document.documentElement.style.display = "block";
-    } else {
-        window.top.location.href = window.location.href;
-    }
-})();
+if (window.top !== window.self) {
+  window.top.location.href = window.location.href;
+}
