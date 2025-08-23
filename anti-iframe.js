a62a2a61c239c7e0829779db36465d4cea351e6c
amouroff <48030869+amouroff@users.noreply.github.com>
2025-08-23 19:23:50 +0300

    Update anti-iframe.js

--- anti-iframe.js
+++ anti-iframe.js
@@ -1,9 +1,7 @@
 (function() {
     if (window.top === window.self) {
-        // Если открыто напрямую — ничего не делаем
         document.documentElement.style.display = "block";
     } else {
-        // Если во фрейме — редиректим
         window.top.location.href = window.location.href;
     }
 })();
