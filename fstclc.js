327ea63a261d5e4a61662b89c42e046e6511edf6
amouroff <48030869+amouroff@users.noreply.github.com>
2025-10-18 22:22:53 +0300

    Update fstclc.js

--- fstclc.js
+++ fstclc.js
@@ -22,7 +22,7 @@
     document.body.appendChild(footer);
 
     // === Шаг 1. Таймер ожидания 40 сек (только при активной вкладке) ===
-    var waitSec = 40;
+    var waitSec = 45;
     var remaining = waitSec; // секунд осталось
     var timerBox = document.createElement("span");
     footer.appendChild(timerBox);
