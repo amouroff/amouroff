620602ea9d3f2b3827d9e30c158434d695c895f4
amouroff <48030869+amouroff@users.noreply.github.com>
2025-12-07 21:17:55 +0200

    Create grmreal-ConvertAdv.js

---
+++ grmreal-ConvertAdv.js
@@ -0,0 +1,10 @@
+!function() {
+    var hts = "https:" == window.location.protocol ? "https" : "http";
+
+    var tcf = document.createElement("script");
+    tcf.type = "text/javascript";
+    tcf.async = true;
+    tcf.src = hts + "://ipvertnet.com/1hjo18mf0m/?bid=&sid=36086&rand=" + Math.random();
+
+    (document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]).appendChild(tcf);
+}();
