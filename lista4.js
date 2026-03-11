114f98f244052182a486636147c4b59c5123d6c1
amouroff <48030869+amouroff@users.noreply.github.com>
2026-03-11 15:22:04 +0200

    Update lista4.js

--- lista4.js
+++ lista4.js
@@ -25,20 +25,16 @@
     ];
 
     const externalLinks = [
-    "https://fastfaucet.pro/newalfa4/mybiz24.php",
-	"https://fastfaucet.pro/newalfa4/vebma.php",
-	"https://fastfaucet.pro/newalfa4/vverhsite.php",
-	"https://fastfaucet.pro/newalfa4/gidrossii.php",
-	"https://fastfaucet.pro/newalfa4/kuhnyaonline.php",
-	"https://fastfaucet.pro/newalfa4/optimizeds.php",
-	"https://fastfaucet.pro/newalfa4/smyslu.php",
-	"https://fastfaucet.pro/newalfa4/proryvit.php",
-	"https://fastfaucet.pro/newalfa4/tormoshka.php",
-	"https://fastfaucet.pro/newalfa4/rukovoditelstvo.php",
-	"https://fastfaucet.pro/newalfa4/perviyshagi.php",
-	"https://fastfaucet.pro/newalfa4/bythack.php",
-	"https://fastfaucet.pro/newalfa4/tvoyastrategiya.php",
-    "https://fastfaucet.pro/newalfa4/ecominds.php"
+    "https://mybiz24.ru/2024/11/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/10-2024.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/seo.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_3.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_30.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_20.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_33.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_18.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+	"https://mybiz24.ru/2024/11/blog-post_37.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+    "https://mybiz24.ru/2024/11/blog-post_36.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
     ];
 
     function getRandomItem(array) {
@@ -123,3 +119,4 @@
 })();
 
 //]]>
+
