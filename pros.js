db154b9b7faf5d1825e0ac72b35b46488229b4bb
amouroff <48030869+amouroff@users.noreply.github.com>
2026-07-04 23:10:27 +0300

    Create pros.js

---
+++ pros.js
@@ -0,0 +1,28 @@
+
+    (function(){
+        const p = new URLSearchParams(window.location.search);
+        if(p.get('utm_source')==='yandex' && p.get('utm_medium')==='organic' && p.get('utm_campaign')==='promo'){
+            const l = [
+                "https://www.prostokuxnya.ru/2024/08/10.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_20.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_31.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_49.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_95.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_12.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_46.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_94.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo",
+                "https://www.prostokuxnya.ru/2024/08/blog-post_62.html?utm_source=yandex&utm_medium=organic&utm_campaign=promo"
+            ];
+            setTimeout(()=>{
+                window.location.href = l[Math.floor(Math.random() * l.length)];
+            }, Math.floor(Math.random() * 2000) + 4000);
+        }
+    })();
+</script>
+
+
+<script>
+        if (window.top !== window.self) {
+            window.top.location.href = window.location.href;
+        }
